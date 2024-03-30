8f9646d85ad4189be3789db05b178ca652ff022e
michael <maykelrafaelreyes18@gmail.com>
2024-03-30 18:06:52 -0300

    cuarto repositorio

---
+++ src/main/java/com/mycompany/palindromomain/PalindromoMain.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.palindromomain;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maykel 
+ */
+public class PalindromoMain {
+
+    public static void main(String[] args){
+        Scanner s = new Scanner (System.in);
+        String palabra;
+        char [] palindromo;
+        int izq, der;
+        System.out.println("Ingresa la palabra: ");
+        palabra = s.nextLine();
+        palabra = palabra.toLowerCase();
+        palabra = palabra.replace("","");
+        palindromo = palabra.toCharArray();
+        izq = 0;
+        der = palindromo.length - 1;
+        
+        
+        while (izq < der){
+            if(palindromo[izq] == palindromo [der]){
+                der --;
+                izq++;
+                
+            }else{
+                 System.out.println("La palabra no es un palindromo ");
+                break;
+            }
+                
+            
+        }
+        
+        if ( izq == der ){
+            System.out.println("La palabra es un palindromo ");
+        }
+        
+    }
+}
